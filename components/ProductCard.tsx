2bb8576d2ad51a9a5ac7906f998be60aa3409315
TeeGreat <toyinatofarati64@gmail.com>
2025-03-31 04:08:48 +0100

    product card

---
+++ components/ProductCard.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import React from "react";
+import AnimatedContainer from "./AnimatedContainer";
+
+interface ProductCardProps {
+  index: number;
+  imgSrc: string;
+  title: string;
+  price: number;
+  isActive: boolean;
+  onClick: () => void;
+}
+
+const ProductCard = ({
+  index,
+  imgSrc,
+  title,
+  price,
+  isActive,
+  onClick,
+}: ProductCardProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`hover:scale-105 ${isActive ? "scale-105" : ""}`}
+    >
+      <AnimatedContainer
+        delay={index * 0.2}
+        styles={`w-80 h-32 flex flex-row gap-4 rounded-xl transition-all duration-300 cursor-pointer ${
+          isActive ? "bg-gradient" : "bg-stone-800"
+        }`}
+      >
+        <Image
+          src={imgSrc}
+          alt="product"
+          width={120}
+          height={120}
+          unoptimized={true}
+          className="rounded-xl"
+        />
+        <div className="flex flex-col justify-between px-4 py-6 text-slate-200">
+          <h3 className="text-lg font-semibold">{title}</h3>
+          <div className="flex flex-row justify-between items-center">
+            <p className="text-slate-400">${price}</p>
+            <div className="w-16 flex justify-center py-1 text-sm border rounded-xl hover:bg-stone-200 hover:text-stone-800 transition duration-300">
+              Buy
+            </div>
+          </div>
+        </div>
+      </AnimatedContainer>
+    </div>
+  );
+};
+
+export default ProductCard;
--- components/sections/Products.tsx
+++ components/sections/Products.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Catalog, { ProductType } from "./Catalog";
+import Preview from "./Preview";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [selectedProduct, setSelectedProduct] = useState<ProductType>({
+    id: "1",
+    imgSrc: "/assets/keyboard1.png",
+    title: "Magic Keyboard",
+    price: 79.99,
+    modelSrc: "/assets/keyboard.glb",
+  });
+
+  const handleProductClick = (product: ProductType) => {
+    setSelectedProduct(product);
+  };
+
+  return (
+    <div>
+      <Catalog
+        onproductClick={handleProductClick}
+        selectedProduct={selectedProduct}
+      />
+      <Preview />
+    </div>
+  );
 };
 
 export default Products;
